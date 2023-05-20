af423e887abbe6e20d62c56a241efa819ba03584
Wizardcn <supawat125947@gmail.com>
2023-05-20 12:46:42 +0700

    withdraw method and refactor

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -2,20 +2,34 @@ package pointersanderrors
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // In Go, when you call a function or a method the arguments are copied.
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+func (w *Wallet) Deposit(amount Bitcoin) {
+	// fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	// *Wallet is called `struct pointers` and they are automatically dereferenced
 	// by convention you should keep your method receiver types the same for consistency.
 
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
