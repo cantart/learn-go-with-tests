ca216ce9d9920e91e975c64d694d6dba838a3622
Wizardcn <supawat125947@gmail.com>
2023-05-20 11:40:32 +0700

    refactor to pass by reference

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -11,7 +11,7 @@ type Triangle struct {
 	Height float64
 }
 
-func (t Triangle) Area() float64 {
+func (t *Triangle) Area() float64 {
 	return (t.Base * t.Height) * 0.5
 }
 
@@ -20,22 +20,18 @@ type Rectangle struct {
 	Height float64
 }
 
-func (r Rectangle) Area() float64 {
+func (r *Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 type Circle struct {
 	Radius float64
 }
 
-func (c Circle) Area() float64 {
+func (c *Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
-}
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
-}
-
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
 }
