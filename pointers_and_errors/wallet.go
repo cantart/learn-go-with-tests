a67b355637d4a6d7535b2f0a5c1a340e40ea70a5
Wizardcn <supawat125947@gmail.com>
2023-05-20 12:24:51 +0700

    introduce struct pointers

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -8,11 +8,14 @@ type Wallet struct {
 
 // In Go, when you call a function or a method the arguments are copied.
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
+	// *Wallet is called `struct pointers` and they are automatically dereferenced
+	// by convention you should keep your method receiver types the same for consistency.
+
 	return w.balance
 }
