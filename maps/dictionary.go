f778b76ff29aa779024c529df3b83262bb6ecbb2
Wizardcn <supawat125947@gmail.com>
2023-05-22 15:43:17 +0700

    refacter

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,15 +1,18 @@
 package dictionary
 
-import (
-	"errors"
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
 type Dictionary map[string]string
 
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
-)
+// create DictionaryErr type which implements the error interface
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
