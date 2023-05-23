eba063a8b1cc87876d867a1000d8defd09144737
Wizardcn <supawat125947@gmail.com>
2023-05-23 11:36:41 +0700

    di use cases

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"learngowithtests/di"
 	"log"
-	"main/dependency_injection/di"
 	"net/http"
 )
 
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-	di.Greet(w, "world")
-}
-
 func main() {
 	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	di.Greet(w, "world")
 }
