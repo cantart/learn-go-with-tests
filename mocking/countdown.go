723491035e5f3e14e68b77993458a82100114709
Wizardcn <supawat125947@gmail.com>
2023-05-23 12:06:17 +0700

    refacter and add time sleep

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,15 +3,24 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
+)
+
+const (
+	countdownStart = 3
+	finalWord      = "Go!"
 )
 
 /*
 This chapter will focus on It's an important skill to be able to slice up requirements as small as you can so you can have working software.
 And, use DI to make function can work on test and terminal
 */
+
+// refactoring some magic values into named constants.
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
