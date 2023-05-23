9f7dfe5a811ec242c44e308a210309fdf90d3111
Wizardcn <supawat125947@gmail.com>
2023-05-23 12:26:49 +0700

    add sleep dependency

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,7 +3,6 @@ package mocking
 import (
 	"fmt"
 	"io"
-	"time"
 )
 
 const (
@@ -11,17 +10,22 @@ const (
 	finalWord      = "Go!"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
 /*
 This chapter will focus on It's an important skill to be able to slice up requirements as small as you can so you can have working software.
 And, use DI to make function can work on test and terminal
 */
 
 // refactoring some magic values into named constants.
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
