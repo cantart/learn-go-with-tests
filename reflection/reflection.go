11d1c68ccb34cdb92826e7a5438e9dbcd7ce4fe8
Wizardcn <supawat125947@gmail.com>
2023-06-05 16:49:55 +0700

    refactor

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -13,20 +13,24 @@ import (
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
 	// When you're doing a comparison on the same value more than once generally refactoring into a switch will improve readability and make your code easier to extend.
 	switch val.Kind() {
-	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
 	case reflect.String:
 		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
 	}
 
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
 }
 
 func getValue(x interface{}) reflect.Value {
