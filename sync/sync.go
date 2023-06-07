56797213ff2d2c9e9c1356c6b39822a12d6c90e8
Wizardcn <supawat125947@gmail.com>
2023-06-07 23:13:23 +0700

    introduce waitgroup and mutex

--- sync/sync.go
+++ sync/sync.go
@@ -1,13 +1,34 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
+// ! Prefer to create a constructor which shows readers of your API that it would be better to not initialise the type yourself.
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
 func (c *Counter) Inc() {
+	c.mu.Lock() // The zero value for a Mutex is an unlocked mutex.
+	defer c.mu.Unlock()
 	c.value++
+
 }
 
 func (c *Counter) Value() int {
 	return c.value
 }
+
+/*
+! Note:
+- Use channels when passing ownership of data
+- Use mutexes for managing state
+- go vet
+- Don't use structs embedding because it's convenient (embedding other struct to struct)
+  by using it we can access the base’s fields directly that means you have exposed your API as public
+*/
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,21 +1,52 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+/*
+! Requirements
+- Can invoke Inc method to count up its value on Counter
+- Must be safe to use in a concurrent environment
+*/
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		assertCount(t, counter, 3)
+		assertCounter(t, counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				defer wg.Done()
+				counter.Inc()
+			}()
+		}
+
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
-func assertCount(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
-		t.Errorf("got %d, want %d", got.Value(), 3)
+		t.Errorf("got %d, want %d", got.Value(), want)
 	}
 }
+
+// ! By waiting for wg.Wait() to finish before making our assertions we can be sure all of our goroutines have attempted to Inc the Counter.
+// ! A Mutex must not be copied after first use. so use pass it to other func by pointer.
