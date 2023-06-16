74f07a89e56a45cb32e234a170b59c3b5340ae7f
Wizardcn <supawat125947@gmail.com>
2023-06-16 12:45:43 +0700

    modify code to a sensible impl

--- context/context.go
+++ context/context.go
@@ -10,9 +10,35 @@ type Store interface {
 	Cancel()
 }
 
+// non-sense implementation
+// func Server(store Store) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		store.Cancel()
+// 		fmt.Fprint(w, store.Fetch())
+// 	}
+// }
+
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		store.Cancel()
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }
+
+/*
+context has a method Done() which returns a channel which gets sent a signal when the context is "done" or "cancelled".
+We want to listen to that signal and call store.Cancel if we get it but we want to ignore it if our Store manages to Fetch before it.
+*/
+// context is goroutines
